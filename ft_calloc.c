3895d279edc73914218336fbdb7c293df355e287
Sebastien Cambier <scambier@bess-f3r6s6.clusters.42paris.fr>
2023-11-12 19:55:00 +0100

    Fixed calloc (wasn't initializing values to zero)

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 17:35:41 by scambier          #+#    #+#             */
-/*   Updated: 2023/11/08 17:46:02 by scambier         ###   ########.fr       */
+/*   Updated: 2023/11/12 17:08:26 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,14 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	size_t	target_size;
+	char	*out;
 
 	target_size = nmemb * size;
 	if (target_size < nmemb || target_size < size)
 		return (0);
-	return (malloc(target_size));
+	out = malloc(target_size);
+	if (!out)
+		return (0);
+	ft_bzero(out, target_size);
+	return (out);
 }
