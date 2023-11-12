d0293fa2257d54400bad46f73f7faa04618f577f
Sebastien Cambier <scambier@bess-f3r6s6.clusters.42paris.fr>
2023-11-12 19:59:51 +0100

    Fixed ft_striteri() (missing header)

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,9 +6,11 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 16:05:51 by scambier          #+#    #+#             */
-/*   Updated: 2023/11/12 16:12:39 by scambier         ###   ########.fr       */
+/*   Updated: 2023/11/12 19:58:21 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
