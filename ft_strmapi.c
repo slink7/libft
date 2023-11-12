770440ec2a4941cf8e3d1a4ab16cebd37199a5e4
Sebastien Cambier <scambier@bess-f3r6s6.clusters.42paris.fr>
2023-11-12 20:00:34 +0100

    Fixed ft_strmapi (infinite loop)

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 15:59:38 by scambier          #+#    #+#             */
-/*   Updated: 2023/11/12 16:12:10 by scambier         ###   ########.fr       */
+/*   Updated: 2023/11/12 16:30:47 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 
 	out_len = ft_strlen(s);
 	out = malloc(sizeof(char) * (out_len + 1));
-	k = 0;
-	while (s[k])
+	k = -1;
+	while (s[++k])
 		out[k] = f(k, s[k]);
 	out[out_len] = '\0';
 	return (out);
