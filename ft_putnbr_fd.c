d6fc9a90236b4df96657fc8566e2697a31bdcd2f
slink <slink794@gmail.com>
2023-11-14 01:11:58 +0100

    Fixed 'ft_putnbr_fd'

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -12,18 +12,23 @@
 
 #include <unistd.h>
 
+#include "libft.h"
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	static char	*dict = "0123456789";
-
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
 	if (n < 0)
 	{
-		write(fd, "-", 1);
-		ft_putnbr_fd(-n / 10, fd);
-		write(fd, dict + n % 10, 1);
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(-(n / 10), fd);
+		ft_putchar_fd('0' + ((-n) % 10), fd);
 		return ;
 	}
 	if (n > 9)
 		ft_putnbr_fd(n / 10, fd);
-	write(fd, dict + n % 10, 1);
+	ft_putchar_fd('0' + (n % 10), fd);
 }
