2f86768e59e0cb669570e8e0b91e9481401575eb
slink <slink794@gmail.com>
2023-11-14 01:12:41 +0100

    Fixed 'ft_split'

--- ft_split.c
+++ ft_split.c
@@ -39,28 +39,6 @@ static int	get_word_len(char const *str, char c)
 	return (k);
 }
 
-static int	min(int a, int b)
-{
-	return ((a < b) * a + (b <= a) * b);
-}
-
-static char	*ft_strndup(const char *str, size_t n)
-{
-	char	*out;
-	int		size;
-
-	size = min(ft_strlen(str), n);
-	out = malloc(sizeof(char) * size);
-	if (!out)
-		return (0);
-	while (size >= 0)
-	{
-		out[size] = str[size];
-		size--;
-	}
-	return (out);
-}
-
 char	**ft_split(char const *s, char c)
 {
 	char	**out;
@@ -70,13 +48,15 @@ char	**ft_split(char const *s, char c)
 
 	out_len = get_count_word(s, c);
 	out = malloc(sizeof(char *) * (out_len + 1));
+	if (!out)
+		return (0);
 	k = 0;
 	while (k < out_len)
 	{
 		while (*s == c)
 			s++;
 		word_len = get_word_len(s, c);
-		out[k] = ft_strndup(s, word_len - 1);
+		out[k] = ft_substr(s, 0, word_len);
 		s += word_len;
 		k++;
 	}
