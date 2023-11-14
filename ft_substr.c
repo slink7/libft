7537f2a8eae5bacfca16911b42d4d6fd15e09af6
slink <slink794@gmail.com>
2023-11-14 01:25:03 +0100

    Fixed 'ft_substr'

--- ft_substr.c
+++ ft_substr.c
@@ -19,18 +19,26 @@ static size_t	min(size_t a, size_t b)
 	return ((a <= b) * a + (a > b) * b);
 }
 
+static size_t	max(size_t a, size_t b)
+{
+	return ((a >= b) * a + (a < b) * b);
+}
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	target_len;
+	size_t	s_len;
 	char	*out;
 	size_t	k;
 
-	target_len = min(len, ft_strlen(s) - start);
-	out = malloc(sizeof(char) * target_len);
+	s_len = ft_strlen(s);
+	target_len = max(min(len, s_len - min(start, s_len)), 0);
+	out = malloc(sizeof(char) * (target_len + 1));
 	if (!out)
 		return (0);
-	k = 0;
-	while (k < len)
+	k = -1;
+	while (++k < target_len)
 		out[k] = s[start + k];
+	out[target_len] = 0;
 	return (out);
 }
