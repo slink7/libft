d38e72a61feff9bdf8e79574d216e536848e6486
slink <slink794@gmail.com>
2023-11-16 01:08:03 +0100

    Fixed ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -24,8 +24,7 @@ void	ft_putnbr_fd(int n, int fd)
 	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
-		ft_putnbr_fd(-(n / 10), fd);
-		ft_putchar_fd('0' + ((-n) % 10), fd);
+		ft_putnbr_fd(-n, fd);
 		return ;
 	}
 	if (n > 9)
