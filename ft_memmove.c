1625127e4afe290f907c0fb6bc770d7c13d91c8e
slink <slink794@gmail.com>
2023-11-16 01:07:34 +0100

    Fixed ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -12,18 +12,18 @@
 
 #include "libft.h"
 
-void    *ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-    size_t    i;
+	size_t	i;
 
 	if (!dest && !src)
-    	return (NULL);
+		return (0);
 	i = -1;
-    if (dest > src)
-        while (n-- >= 1)
-            ((char *)dest)[n] = ((char *)src)[n];
-    else
+	if (dest > src)
+		while (n-- >= 1)
+			((char *)dest)[n] = ((char *)src)[n];
+	else
 		while (++i < n)
-            ((char *)dest)[i] = ((char *)src)[i];
-    return (dest);
+			((char *)dest)[i] = ((char *)src)[i];
+	return (dest);
 }
