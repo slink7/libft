d434f00f3cfe1cd51eb1ee3213f600a153bb253f
Sebastien Cambier <scambier@student.42.fr>
2024-01-26 14:07:25 +0100

    Added ft_atoi_base

---
+++ str/ft_atoi_base.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/26 14:00:50 by scambier          #+#    #+#             */
+/*   Updated: 2024/01/26 14:06:13 by scambier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strchri(char *str, char c)
+{
+	return (ft_strchr(str, c) - str);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int		ibase;
+	int		sign;
+	int		out;
+
+	ibase = ft_strlen(base);
+	sign = 1;
+	out = 0;
+	while ((*str && *str >= 9 && *str <= 13) || *str == ' ')
+		str++;
+	while (*str && (*str == '+' || *str == '-'))
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (*str && ft_strchri(base, *str) >= 0)
+	{
+		out *= ibase;
+		out += ft_strchri(base, *str);
+		str++;
+	}
+	return (sign * out);
+}
