2c0511e6e5c242afc3ab216680f0225423c278f2
Sebastien Cambier <scambier@student.42.fr>
2024-01-26 14:06:58 +0100

    Fixed wrong header define

--- ftmath.h
+++ ftmath.h
@@ -6,12 +6,12 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 16:48:19 by scambier          #+#    #+#             */
-/*   Updated: 2024/01/25 16:40:37 by scambier         ###   ########.fr       */
+/*   Updated: 2024/01/26 14:06:30 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MATH_H
-# define MATH_H
+#ifndef FTMATH_H
+# define FTMATH_H
 
 int		ft_max(int a, int b);
 int		ft_min(int a, int b);
