1835b84d9bd6ae16c417004e0caede86822c30e1
scambier <scambier@student.42.fr>
2024-03-07 15:14:02 +0100

    Fixed atoi_strict fr

--- str/ft_atoi_strict.c
+++ str/ft_atoi_strict.c
@@ -6,21 +6,22 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 17:47:01 by scambier          #+#    #+#             */
-/*   Updated: 2024/03/07 14:38:03 by scambier         ###   ########.fr       */
+/*   Updated: 2024/03/07 15:13:14 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+#define INT_MAX 2147483647l
+#define INT_MIN -2147483648l
+
 int	ft_atoi_strict(int *out, char *in)
 {
-	int	sign;
-	int	len;
+	int		sign;
+	long	nbr;
 
 	*out = 0;
-	len = ft_strlen(in);
-	if (len > 10 || (len == 10 && ft_strncmp("2147483648", in, 12) <= 0))
-		return (0);
+	nbr = 0;
 	sign = 1;
 	if (*in == '-' && in++)
 		sign = -1;
@@ -28,9 +29,12 @@ int	ft_atoi_strict(int *out, char *in)
 	{
 		if (!ft_isdigit(*in))
 			return (0);
-		*out = *out * 10 + *in - '0';
+		nbr = nbr * 10 + *in - '0';
 		in++;
 	}
-	*out *= sign;
+	nbr *= sign;
+	if (nbr < INT_MIN || nbr > INT_MAX)
+		return (0);
+	*out = nbr;
 	return (1);
 }
