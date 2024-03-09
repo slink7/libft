30cda093c925754522543502450d5127c1e0ceac
Sebastien Cambier <scambier@student.42.fr>
2024-03-10 00:38:18 +0100

    Fixed segfault when strbuilder_addstr had empty str & null len

--- strbuilder/ft_strbuilder_addstr.c
+++ strbuilder/ft_strbuilder_addstr.c
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 18:47:54 by scambier          #+#    #+#             */
-/*   Updated: 2024/03/10 00:29:00 by scambier         ###   ########.fr       */
+/*   Updated: 2024/03/10 00:36:30 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_strbuilder_addstr(t_strbuilder *buffer, char *str, int len)
 {
-	if (!str || len < 0)
+	if (!str || len < 0 || !*str)
 		return ;
 	if (len > 0)
 		while (*str && len--)
