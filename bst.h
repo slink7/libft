d5d106878ebc36bb6610554a67809b23d0e0c8c0
Sebastien Cambier <slink794@gmail.com>
2024-03-21 06:50:54 +0100

    Fixed type error

--- bst.h
+++ bst.h
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 02:31:40 by scambier          #+#    #+#             */
-/*   Updated: 2024/03/19 19:04:28 by scambier         ###   ########.fr       */
+/*   Updated: 2024/03/21 06:48:43 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	ft_var_free(t_var **var);
 t_bst	*ft_bst_new(t_var	*var);
 void	ft_bst_free(t_bst **bst);
 t_bst	*ft_bst_fetch(t_bst **bst, char *name);
-void	*ft_bst_getvar(t_bst *bst, char *name);
+char	*ft_bst_getvar(t_bst *bst, char *name);
 void	ft_bst_setvar(t_bst **bst, char *name, char *value);
 void	ft_bst_print(t_bst *bst);
 int		ft_bst_size(t_bst *bst);
--- bst/ft_bst_getvar.c
+++ bst/ft_bst_getvar.c
@@ -6,13 +6,13 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 02:40:09 by scambier          #+#    #+#             */
-/*   Updated: 2024/03/19 18:37:42 by scambier         ###   ########.fr       */
+/*   Updated: 2024/03/21 06:47:07 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_bst_getvar(t_bst *bst, char *name)
+char	*ft_bst_getvar(t_bst *bst, char *name)
 {
 	return (ft_bst_fetch(&bst, name)->var->value);
 }
