c3e4239da99c8882b57cc3bbc852d479a147f17b
Sebastien Cambier <slink794@gmail.com>
2024-04-07 01:04:22 +0200

    Added return to strbuilder_addstr

--- strbuilder.h
+++ strbuilder.h
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 18:34:49 by scambier          #+#    #+#             */
-/*   Updated: 2024/03/10 00:29:14 by scambier         ###   ########.fr       */
+/*   Updated: 2024/04/07 01:03:44 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ typedef struct s_strbuilder {
 t_strbuilder	*ft_strbuilder_new(void);
 int				ft_strbuilder_free(t_strbuilder **buffer);
 void			ft_strbuilder_addchar(t_strbuilder *buffer, char c);
-void			ft_strbuilder_addstr(t_strbuilder *buffer, char *str, int len);
+int				ft_strbuilder_addstr(t_strbuilder *buffer, char *str, int len);
 int				ft_strbuilder_len(t_strbuilder *builder);
 char			*ft_strbuilder_build(t_strbuilder *buffer);
 
--- strbuilder/ft_strbuilder_addstr.c
+++ strbuilder/ft_strbuilder_addstr.c
@@ -6,17 +6,18 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 18:47:54 by scambier          #+#    #+#             */
-/*   Updated: 2024/04/06 16:07:31 by scambier         ###   ########.fr       */
+/*   Updated: 2024/04/07 01:03:32 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_strbuilder_addstr(t_strbuilder *buffer, char *str, int len)
+int	ft_strbuilder_addstr(t_strbuilder *buffer, char *str, int len)
 {
 	if (!str || len < 0 || !*str)
-		return ;
+		return (0);
 	if (len > 0)
 		while (*str && len--)
 			ft_strbuilder_addchar(buffer, *str++);
+	return (1);
 }
