19a131141f3ec1d3fe0c906fa2eedc552e8ebd54
Sebastien Cambier <slink794@gmail.com>
2024-05-10 16:01:05 +0200

    Fixed get_file

--- stream.h
+++ stream.h
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 16:42:04 by scambier          #+#    #+#             */
-/*   Updated: 2024/05/08 19:20:09 by scambier         ###   ########.fr       */
+/*   Updated: 2024/05/10 15:50:44 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int		ft_putchar_fd(char c, int fd);
 int		ft_putstr_fd(char *s, int fd);
 int		ft_putnbr_fd(int n, int fd);
 int		ft_putnbr_base_fd(long nbr, char *base, int fd);
-char	**ft_get_file(char *path);
+char	*ft_get_file(char *path, unsigned int buffer_size);
 char	*get_next_line(int fd);
 
 #endif
--- stream/ft_get_file.c
+++ stream/ft_get_file.c
@@ -6,35 +6,29 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 19:17:54 by scambier          #+#    #+#             */
-/*   Updated: 2024/05/08 19:19:39 by scambier         ###   ########.fr       */
+/*   Updated: 2024/05/10 15:58:13 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
 
 #include "libft.h"
 
-char	**ft_get_file(char *path)
+char	*ft_get_file(char *path, unsigned int buffer_size)
 {
-	t_list	*lines;
-	char	*line;
 	int		fd;
-	char	**out;
+	char	*out;
+	char	*buffer;
 
 	fd = open(path, O_RDONLY);
 	if (fd < 3)
 		return (0);
-	lines = 0;
-	line = get_next_line(fd);
-	while (line)
-	{
-		line[ft_strlen(line) - 1] = 0;
-		ft_lstadd_back(&lines, ft_lstnew(line));
-		line = get_next_line(fd);
-	}
-	close(fd);
-	out = (char **)ft_lstexport(lines);
-	ft_lstclear(&lines, 0);
+	out = ft_calloc(1, sizeof(char));
+	buffer = ft_calloc(buffer_size + 1, sizeof(char));
+	while (read(fd, buffer, buffer_size))
+		ft_strcat(&out, buffer);
+	free(buffer);
 	return (out);
 }
