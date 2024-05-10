46073e9f89185865f5a895ec549e1815dab16c8b
Sebastien Cambier <slink794@gmail.com>
2024-05-10 19:09:55 +0200

    Fixed major pointer error

--- mem/ft_memcat.c
+++ mem/ft_memcat.c
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 18:50:31 by scambier          #+#    #+#             */
-/*   Updated: 2024/05/10 18:54:36 by scambier         ###   ########.fr       */
+/*   Updated: 2024/05/10 19:07:07 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_memcat(void **dst, int dst_len, void *src, int src_len)
 	void	*temp;
 
 	temp = ft_calloc(dst_len + src_len, sizeof(unsigned char));
-	ft_memcpy(temp, dst, dst_len);
+	ft_memcpy(temp, *dst, dst_len);
 	ft_memcpy(temp + dst_len, src, src_len);
 	free(*dst);
 	*dst = temp;
