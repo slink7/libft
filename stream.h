f6a743f8128b97217a95f39302795ba25c2999c2
Sebastien Cambier <slink794@gmail.com>
2024-05-28 16:41:54 +0200

    Added ft_bindump

--- stream.h
+++ stream.h
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 16:42:04 by scambier          #+#    #+#             */
-/*   Updated: 2024/05/10 17:38:45 by scambier         ###   ########.fr       */
+/*   Updated: 2024/05/28 16:12:27 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ int		ft_putnbr_fd(int n, int fd);
 int		ft_putnbr_base_fd(long nbr, char *base, int fd);
 int		ft_get_file(char **dst, char *path, unsigned int buffer_size);
 char	*get_next_line(int fd);
+void	ft_bindump(char *ptr, int len, int bpl);
 
 #endif
---
+++ stream/ft_bindump.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bindump.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 15:38:51 by scambier          #+#    #+#             */
+/*   Updated: 2024/05/28 16:38:34 by scambier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+#include "libft.h"
+
+#define HEX "0123456789ABCDEF"
+
+void	ppp(char *ptr, int len, int bpl)
+{
+	char	*buffer;
+	int		k;
+	int		l;
+
+	buffer = ft_calloc(bpl * 13 + 3, sizeof(char *));
+	ft_memset(buffer, 32, bpl * 13 + 2);
+	buffer[bpl * 11 - 1] = '|';
+	buffer[bpl * 13] = '|';
+	buffer[bpl * 13 + 1] = '\n';
+	k = -1;
+	while (++k < len * 8)
+	{
+		l = 8 * (k / 8) + (7 - (k % 8));
+		buffer[l + l / 4 + l / 8] = ((ptr[k / 8] >> (k % 8)) & 1) + '0';
+	}
+	k = -1;
+	while (++k < ft_min(bpl, len) * 2)
+	{
+		if (k % 2)
+			buffer[k + bpl * 11] = HEX[ptr[k / 2] & 0b1111];
+		else
+			buffer[k + bpl * 11] = HEX[(ptr[k / 2] >> 4) & 0b1111];
+	}
+	write(1, buffer, bpl * 13 + 2);
+	free(buffer);
+}
+
+void	ft_bindump(char *ptr, int len, int bpl)
+{
+	while (len > 0)
+	{
+		ppp(ptr, ft_min(len, bpl), bpl);
+		ptr += bpl;
+		len -= bpl;
+	}
+}
