875980ef3e0897a3407283b64dae5cb4c3e0bc75
Sebastien Cambier <slink794@gmail.com>
2024-05-28 18:08:16 +0200

    Added ft_hexdump

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: scambier <scambier@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/11/08 17:06:10 by scambier          #+#    #+#              #
-#    Updated: 2024/05/28 15:48:48 by scambier         ###   ########.fr        #
+#    Updated: 2024/05/28 17:39:40 by scambier         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -76,6 +76,7 @@ SRC = \
 	stream/ft_putnbr_base_fd.c\
 	stream/ft_get_file.c\
 	stream/ft_bindump.c\
+	stream/ft_hexdump.c\
 	math/ft_min.c\
 	math/ft_max.c\
 	math/ft_abs.c\
--- stream/ft_hexdump.c
+++ stream/ft_hexdump.c
@@ -6,11 +6,52 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 17:25:45 by scambier          #+#    #+#             */
-/*   Updated: 2024/05/28 17:26:07 by scambier         ###   ########.fr       */
+/*   Updated: 2024/05/28 18:07:17 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_bindump(char *ptr, int len, int bpl)
+#include <unistd.h>
+#include <stdlib.h>
+
+#include "libft.h"
+
+#define HEX "0123456789ABCDEF"
+
+static void	put_hex_line(char *ptr, int len, int bpl)
 {
-	
+	char	*buffer;
+	int		k;
+	int		l;
+
+	buffer = ft_calloc(bpl * 4 + 3, sizeof(char *));
+	ft_memset(buffer, 32, bpl * 4);
+	buffer[bpl * 4 + 1] = '\n';
+	buffer[bpl * 3 - 1] = '|';
+	buffer[bpl * 4] = '|';
+	k = -1;
+	while (++k < len * 2)
+	{
+		l = 2 * (k / 2) + 1 - k % 2;
+		buffer[l + l / 2] = HEX[(ptr[k / 2] >> (k % 2) * 4) & 0b1111];
+	}
+	k = -1;
+	while (++k < len)
+	{
+		if (ft_isprint(ptr[k]))
+			buffer[bpl * 3 + k] = ptr[k];
+		else
+			buffer[bpl * 3 + k] = '.';
+	}
+	write(1, buffer, bpl * 4 + 3);
+	free(buffer);
+}
+
+void	ft_hexdump(char *ptr, int len, int bpl)
+{
+	while (len > 0)
+	{
+		put_hex_line(ptr, ft_min(len, bpl), bpl);
+		ptr += bpl;
+		len -= bpl;
+	}
 }
